97003c5f2268ae1b04720c40f0d9cc472a039801
Daveworld23 <davidbassey23@gmail.com>
2024-01-04 14:41:26 -0800

    sorting errors

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,20 +3,20 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
     argc = len(argv)
-    if argc != 3:
+    if argc != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(argv[1])
     ops = argv[2]
     b = int(argv[3])
     if ops == '+':
-        print("{a} {ops} {b} = {add(a, b)}")
+        print(f"{a} {ops} {b} = {add(a, b)}")
     elif ops == '-':
-        print("{a} {ops} {b} = {sub(a, b)}")
+        print(f"{a} {ops} {b} = {sub(a, b)}")
     elif ops == '*':
-        print("{a} {ops} {b} = {mul(a, b)}")
+        print(f"{a} {ops} {b} = {mul(a, b)}")
     elif ops == '/':
-        print("{a} {ops} {b} = {div(a, b)}")
+        print(f"{a} {ops} {b} = {div(a, b)}")
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
