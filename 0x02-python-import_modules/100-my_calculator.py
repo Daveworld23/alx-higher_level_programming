6d458f4fafe7519ca822951e658c2f647d600636
Daveworld23 <davidbassey23@gmail.com>
2024-01-04 14:30:30 -0800

    my own calculatir

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    fron sys import argv
+    argc = len(argv)
+    if argc != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    ops = argv[2]
+    b = int(argv[3])
+    if ops == '+':
+        print(f"{a} {ops} {b} = {add(a, b)}")
+    elif ops == '-':
+        print(f"{a} {ops} {b} = {sub(a, b)}")
+    elif ops == '*':
+        print(f"{a} {ops} {b} = {mul(a, b)}")
+    elif ops == '/':
+        print(f"{a} {ops} {b} = {div(a, b)}")
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
