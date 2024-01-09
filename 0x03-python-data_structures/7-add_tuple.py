31aa90fceabeeb93f1ac409cf95db78a95ca2b39
Daveworld23 <davidbassey23@gmail.com>
2024-01-09 10:05:06 -0800

    changes to some tasks

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     main_tuple = ()
-    tuple_1 += (0, 0)
-    tuple_2 += (0, 0)
-    main_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_1[2]
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    main_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[2]
     return main_tuple
