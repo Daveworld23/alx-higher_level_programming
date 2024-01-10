4d212e8b3cbaca9a14ee1665d713f029156be12f
Daveworld23 <davidbassey23@gmail.com>
2024-01-10 14:46:21 -0800

    python data structures project

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dic a_dictionary.copy()
+    lists = list(new_dic.keys())
+    for i in lists:
+        new_dic[i] *= 2
+    return (new_dic)
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+This is the Readme file for 0x04-python more data structures
