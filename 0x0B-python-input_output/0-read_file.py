ba8aa0f0b11bb47babae89d5bdefc0aa86d099c1
Daveworld23 <davidbassey23@gmail.com>
2024-02-06 14:33:17 -0800

    debug task 1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" defines a function that reads a text-file"""
 def read_file(filename=""):
+    """ prints the content of the text-file(UTF8) to stdout """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
