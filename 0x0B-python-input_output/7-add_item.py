75601fa7c905bb2b65acb0501356962b38dc9399
Daveworld23 <davidbassey23@gmail.com>
2024-02-07 03:51:22 -0800

    debugging still

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,14 +7,14 @@ import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-    old = list(sys.argv[1:])
-    """giving it arguments."""
+    load_from_json_file = \
+            __import__('6-load_from_json_file').load_from_json_file
+
     try:
-        new = load_from_json_file('add_item.json')
+        new = load_from_json_file("add_item.json")
     except FileNotFoundError:
         new = []
         """if the doesn't exist, load empty file"""
-    new.extend(old)
+    new.extend(sys.argv[1:])
     """extend the list and save the file afterwards"""
-    save_to_json_file(new, 'add_item.json')
+    save_to_json_file(new, "add_item.json")
