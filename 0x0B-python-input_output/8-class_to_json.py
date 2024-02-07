db06edf0666adef1feb6cbfc0ff34a61e1364c76
Daveworld23 <davidbassey23@gmail.com>
2024-02-06 17:01:07 -0800

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Defines a class to JSON function."""
+
+
 def class_to_json(obj):
-    ""Returns the dictionary representation of the data."""
+    """Returns the dictionary representation of the data."""
     return obj.__dict__
