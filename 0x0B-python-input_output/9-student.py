899c27f1399018d882218f3ff9cfaa7cda1af090
Daveworld23 <davidbassey23@gmail.com>
2024-02-06 17:06:29 -0800

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """Defines a class."""
+
+
 class Student:
     """Represents a class called Student."""
     def _init__(self, first_name,last_name, age):
-        """
-        Initializes a new Student.
-        Args:
-            first_name (str): first name of student
-            last_name (str): last nane of student
-            age (int): age of student.
-            """
+        """Initializes the student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+    
     def to_json(self):
         """
         Returns the dictionary representation of the student.
