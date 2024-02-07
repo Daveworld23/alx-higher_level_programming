1adce76db3fda96f7c772428cac58c67ff7fe8c8
Daveworld23 <davidbassey23@gmail.com>
2024-02-06 17:19:47 -0800

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""Definesa class Student."""
+"""Defines a class Student."""
+
+
 class Student:
     """Represents a student."""
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes a new Student.
-        Args:
-            first_name (str): first name of Student
-            last_name (str): last nane of student
-            age (int): age of student.
-        """
+        """Initializes a new Student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self, attrs=None):
         """Gets dictionary representation of student."""
         if attrs is None:
