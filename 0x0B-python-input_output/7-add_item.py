2272c367b1fc9bb4c421c9c995c060faf2f067f0
Daveworld23 <davidbassey23@gmail.com>
2024-02-07 03:34:39 -0800

    changes done

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,15 +5,16 @@
 import sys
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-old = list(sys.argv[1:])
-"""giving it arguments."""
-try:
-    new = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    new = []
-    """if the doesn't exist, load empty file"""
-new.extend(old)
-"""extend the list and save the file afterwards"""
-save_to_json_file(new, 'add_item.json')
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    old = list(sys.argv[1:])
+    """giving it arguments."""
+    try:
+        new = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        new = []
+        """if the doesn't exist, load empty file"""
+    new.extend(old)
+    """extend the list and save the file afterwards"""
+    save_to_json_file(new, 'add_item.json')
