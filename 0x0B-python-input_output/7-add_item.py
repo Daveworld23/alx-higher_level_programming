2e0a963bb0881a38e69aacfa198b6c2539baa4aa
Daveworld23 <davidbassey23@gmail.com>
2024-02-06 20:58:54 -0800

    final

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,11 +8,12 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 old = list(sys.argv[1:])
-
+"""giving it arguments."""
 try:
     new = load_from_json_file('add_item.json')
 except FileNotFoundError:
     new = []
-
+    """if the doesn't exist, load empty file"""
 new.extend(old)
+"""extend the list and save the file afterwards"""
 save_to_json_file(new, 'add_item.json')
