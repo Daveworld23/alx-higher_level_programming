e798c7611babfe29372093005c4962a10103611d
Daveworld23 <davidbassey23@gmail.com>
2024-02-06 19:20:39 -0800

    sleep

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Add arguments to a list and safe them to a file."""
+"""Add arguments to a list and save them to a file."""
 
 
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 items = list(sys.argv[1:])
 
