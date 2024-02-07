2c7f9f36adc967769abab50ff0d0d6afb16fb691
Daveworld23 <davidbassey23@gmail.com>
2024-02-06 17:15:23 -0800

    debugs

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,9 +9,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self):
-        """
-        Returns the dictionary representation of the student.
-        """
+        """Returns the dictionary representation of the student."""
         return self.__dict__
