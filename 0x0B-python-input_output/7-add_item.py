95174294eb36d4804ce99eddfaf8a6385e623335
Daveworld23 <davidbassey23@gmail.com>
2024-02-06 20:32:43 -0800

    last

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,7 +5,7 @@
 import sys
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_fi_e
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 items = list(sys.argv[1:])
