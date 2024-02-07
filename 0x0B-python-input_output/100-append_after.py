07d6b6e56e7a3c72787cca2f2dfdd9e729ccc566
Daveworld23 <davidbassey23@gmail.com>
2024-02-07 04:48:53 -0800

    final debug

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,15 +4,12 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """creates the text file."""
-    text = ""
-    with open(filename) as r:
+    text = []
+    with open(filename, 'r') as r:
         """open file in read mode."""
         for line in r:
             text.append(line)
-            """iterates through the list and appends to text."""
             if search_string in line:
                 text.append(new_string + '\n')
-                """search string and appends new string after current line."""
-    with open(filename, 'w') as w:
-        """writes the modified line back to the file."""
-        w.write(text)
+    with open(filename, 'w') as r:
+        w.writeline(text)
