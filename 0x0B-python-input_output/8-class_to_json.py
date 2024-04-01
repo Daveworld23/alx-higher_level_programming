e9d46bdb4efecbc30d6a1c38f1962a8a3e041f4e
Daveworld23 <138708980+Daveworld23@users.noreply.github.com>
2024-04-01 11:06:37 +0100

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Defines a class to JSON function."""
-
-
-def class_to_json(obj):
-    """Returns the dictionary representation of the data."""
-    return obj.__dict__
--- 0x0B-python-input_output/README.md
+++
@@ -1,1 +0,0 @@
-This is the README file for python input/output project.
